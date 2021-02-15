536cf7695c56c4d655a3e0e899b7b2065d06b8a5
chiknas <nkuknas@gmail.com>
2021-02-15 20:11:45 +0000

    update docker file to run build on docker image creation

--- src/test/java/com/chiknas/swancloudserver/repositories/CursorUtilsTest.java
+++ src/test/java/com/chiknas/swancloudserver/repositories/CursorUtilsTest.java
@@ -27,21 +27,21 @@ class CursorUtilsTest {
 
     @Test
     void toBase64() {
-        assertEquals("rO0ABXNyAFRjb20uY2hpa25hcy5zd2FuY2xvdWRzZXJ2ZXIucmVwb3NpdG9yaWVzLmN1cnNvcnBhZ2luYXRpb24u" +
-                        "Y3Vyc29ycy5GaWxlTWV0YWRhdGFDdXJzb3JanhoASWlMegIAAkwAC2NyZWF0ZWREYXRldAAVTGphdmEvdGltZS9Mb2Nhb" +
-                        "ERhdGU7TAACaWR0ABNMamF2YS9sYW5nL0ludGVnZXI7eHBzcgANamF2YS50aW1lLlNlcpVdhLobIkiyDAAAeHB3BwMAAA" +
-                        "flAgt4c3IAEWphdmEubGFuZy5JbnRlZ2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAU=",
+        assertEquals("rO0ABXNyAFRjb20uY2hpa25hcy5zd2FuY2xvdWRzZXJ2ZXIucmVwb3NpdG9yaWVzLmN1cnNvcnBhZ2luYXRpb24uY" +
+                        "3Vyc29ycy5GaWxlTWV0YWRhdGFDdXJzb3JanhoASWlMegIAAkwAC2NyZWF0ZWREYXRldAAVTGphdmEvdGltZS9Mb2NhbER" +
+                        "hdGU7TAACaWR0ABNMamF2YS9sYW5nL0ludGVnZXI7eHBzcgANamF2YS50aW1lLlNlcpVdhLobIkiyDAAAeHB3BwMAAAflAg" +
+                        "94c3IAEWphdmEubGFuZy5JbnRlZ2VyEuKgpPeBhzgCAAFJAAV2YWx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAU=",
                 CursorUtils.toBase64(cursor));
     }
 
     @Test
     void base64ToCursor() {
         FileMetadataCursor deserializedCursor =
-                (FileMetadataCursor) CursorUtils.base64ToCursor("rO0ABXNyAFRjb20uY2hpa25hcy5zd2FuY2xvdWRzZXJ" +
-                        "2ZXIucmVwb3NpdG9yaWVzLmN1cnNvcnBhZ2luYXRpb24uY3Vyc29ycy5GaWxlTWV0YWRhdGFDdXJzb3JanhoASWlMegIAA" +
-                        "kwAC2NyZWF0ZWREYXRldAAVTGphdmEvdGltZS9Mb2NhbERhdGU7TAACaWR0ABNMamF2YS9sYW5nL0ludGVnZXI7eHBzcgA" +
-                        "NamF2YS50aW1lLlNlcpVdhLobIkiyDAAAeHB3BwMAAAflAgt4c3IAEWphdmEubGFuZy5JbnRlZ2VyEuKgpPeBhzgCAAFJAA" +
-                        "V2YWx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAU=");
+                (FileMetadataCursor) CursorUtils.base64ToCursor("rO0ABXNyAFRjb20uY2hpa25hcy5zd2FuY2xvdWRzZXJ2" +
+                        "ZXIucmVwb3NpdG9yaWVzLmN1cnNvcnBhZ2luYXRpb24uY3Vyc29ycy5GaWxlTWV0YWRhdGFDdXJzb3JanhoASWlMegIAAkw" +
+                        "AC2NyZWF0ZWREYXRldAAVTGphdmEvdGltZS9Mb2NhbERhdGU7TAACaWR0ABNMamF2YS9sYW5nL0ludGVnZXI7eHBzcgANam" +
+                        "F2YS50aW1lLlNlcpVdhLobIkiyDAAAeHB3BwMAAAflAg94c3IAEWphdmEubGFuZy5JbnRlZ2VyEuKgpPeBhzgCAAFJAAV2Y" +
+                        "Wx1ZXhyABBqYXZhLmxhbmcuTnVtYmVyhqyVHQuU4IsCAAB4cAAAAAU=");
         assertEquals(cursor.getId(), deserializedCursor.getId());
         assertEquals(cursor.getCreatedDate(), deserializedCursor.getCreatedDate());
     }
