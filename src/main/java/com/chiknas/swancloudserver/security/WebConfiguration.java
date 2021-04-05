846fe67eea0d3bfebd656a8d51145c978b4efc12
chiknas <nkuknas@gmail.com>
2021-04-05 20:42:59 +0100

    block all requests other than GET and POST

---
+++ src/main/java/com/chiknas/swancloudserver/security/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.chiknas.swancloudserver.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author nkukn
+ * @since 4/5/2021
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestMethodInterceptor()).addPathPatterns("/**");
+    }
+}
