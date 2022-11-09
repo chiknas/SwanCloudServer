99b4edaa921521feff62c99efe5a18be2513853b
Nikos Kyknas <nkuknas@gmail.com>
2022-11-09 20:58:37 +0000

    Configure security for each part of the system separately

--- swancloud-security/src/main/java/com/chiknas/swancloudserver/SecurityConfiguration.java
+++ swancloud-security/src/main/java/com/chiknas/swancloudserver/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -31,29 +33,81 @@ public class SecurityConfiguration {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    /**
+     * Security configuration for the web application. This includes all MVC endpoints
+     * at the main context (starting with /).
+     */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic().disable()
-//                Enable to work with postman
-//                .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+    @Order(3)
+    public SecurityFilterChain webAppFilterChain(HttpSecurity http) throws Exception {
+        return withHttpSecurity(http)
+                .antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/login", "/img/**", "/css/**", "/access-denied", "/auth/signin")
+                .antMatchers("/login", "/img/**", "/css/**", "/access-denied")
                 .permitAll()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .authenticationProvider(authProvider())
                 .formLogin()
                 .loginPage("/login")
                 .and()
                 .exceptionHandling()
                 .accessDeniedPage("/access-denied")
-                .and()
-                .apply(new JwtSecurityConfigurerAdapter(jwtTokenProvider)).and()
-                .build();
+                .and().build();
+    }
+
+    /**
+     * Security configuration for the rest api part of the application. This includes all rest endpoints under
+     * the api context (starting with /api)
+     */
+    @Bean
+    @Order(2)
+    public SecurityFilterChain restApiFilterChain(HttpSecurity http) throws Exception {
+        return withRestHttpSecurity(http)
+                .antMatcher("/api/**")
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().build();
+    }
+
+    /**
+     * Security configuration for the authentication endpoints of the app. This includes all endpoints under
+     * the authentication context (starting with /auth)
+     */
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authenticationFilterChain(HttpSecurity http) throws Exception {
+        return withRestHttpSecurity(http)
+                .antMatcher("/auth/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth/signin")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+
+    /**
+     * Http security configuration for every single HTTP endpoint in the system.
+     */
+    private HttpSecurity withHttpSecurity(HttpSecurity http) throws Exception {
+        return http
+                .authenticationProvider(authProvider())
+                .apply(new JwtSecurityConfigurerAdapter(jwtTokenProvider))
+                .and();
+    }
+
+    /**
+     * Http security for REST endpoints in the system. Basic http security {@link SecurityConfiguration#withHttpSecurity}
+     * rules apply too.
+     */
+    private HttpSecurity withRestHttpSecurity(HttpSecurity http) throws Exception {
+        return withHttpSecurity(http)
+//              Enable for POST requests to work with postman
+//                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
     }
 
 
